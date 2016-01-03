2eac08c2494a36712b6d60db614c8789c6afe4d6
Abe Vos <abe_vos@msn.com>
2016-01-03 13:51:24 +0100

    Started on new shadow collision system

---
+++ Assets/CasterLamp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CasterLamp : MonoBehaviour
+{
+	private Camera camera;
+	private Material material;
+
+	// Creates a private material used to the effect
+	void OnEnable ()
+	{
+		camera = gameObject.AddComponent<Camera> ();
+		camera.orthographic = true;
+
+		material = new Material( Shader.Find("Hidden/PlayerDiscriminatorShader") );
+		camera.depthTextureMode = DepthTextureMode.Depth;
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit (source, destination, material);
+	}
+}
