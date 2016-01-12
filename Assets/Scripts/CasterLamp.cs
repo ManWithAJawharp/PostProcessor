0bc99441df0f59764ad649eeeada9f1ad4ced98a
Abe Vos <abe_vos@msn.com>
2016-01-12 18:06:38 +0100

    Optimize algortihm significantly

--- Assets/Scripts/CasterLamp.cs
+++ Assets/Scripts/CasterLamp.cs
@@ -12,8 +12,8 @@ public class CasterLamp : MonoBehaviour
 	[SerializeField]
 	private GameObject quad;
 
-    [SerializeField, Tooltip("How many frames have to have passed before another calculation is made.")]
-    int regularity = 1;
+    [SerializeField, Tooltip("Time (s) to wait before RenderTexture is being read from gpu.")]
+    float holdingTime = 0.05f;
     [SerializeField, Tooltip("Resolution of RenderTexture.")]
     private int resolution = 16;
     [SerializeField, Range(0, 1), Tooltip("Allowed ratio between between lit and unlit pixels on the player.")]
@@ -46,6 +46,17 @@ public class CasterLamp : MonoBehaviour
     /// </summary>
     private Texture2D resultImage;
 
+    /// <summary>
+    /// True if a texture has been rendered unto, but isn't analysed yet.
+    /// </summary>
+    private bool isHoldingTexture;
+    /// <summary>
+    /// Time in seconds the RenderTexture is being held.
+    /// </summary>
+    private float timeHoldingTexture;
+
+    private Color targetColor;
+
 	void OnEnable ()
 	{
         Debug.Log("Enable " + name);
@@ -56,7 +67,7 @@ public class CasterLamp : MonoBehaviour
 		lampCamera.orthographic = true;
 		lampCamera.enabled = false;
 
-		//  Create Mask Camera object and add Camera Component.
+		//  Create Camera object and add Camera Component.
 		renderCameraObject = new GameObject("Render Camera");
 		renderCameraObject.hideFlags = HideFlags.HideAndDontSave;
 		renderCameraObject.transform.parent = transform;
@@ -64,27 +75,57 @@ public class CasterLamp : MonoBehaviour
 		renderCamera = renderCameraObject.AddComponent<Camera>();
 		renderCamera.enabled = false;
 
+        //  Initialize Texture2D, after reading the RenderTexture to this, the pixels can be read.
         resultImage = new Texture2D(resolution, resolution, TextureFormat.RGB24, false);
         resultImage.filterMode = FilterMode.Point;
         resultImage.wrapMode = TextureWrapMode.Clamp;
+
+        isHoldingTexture = false;
+        timeHoldingTexture = 0f;
+
+        targetColor = Color.red;
+    }
+
+    void Update ()
+    {
+        player.GetComponent<Renderer>().material.color = Color.Lerp(player.GetComponent<Renderer>().material.color, targetColor, Time.deltaTime * 35);
     }
 
 	void LateUpdate()
 	{
         LockOntoPlayer(player);
+        
+        if (renderTex == null)
+        {
+            RenderLightView();
 
-        player.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, RenderLightView());
-    }
+            if (holdingTime == 0f)
+            {
+                player.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, AnalyseTexture());
 
-    void OnPreRender ()
-    {
-        //Debug.Log("PreRender");
+                ReleaseTexture();
+            }
+        }
+        else
+        {
+
+            if (timeHoldingTexture >= holdingTime)
+            {
+                timeHoldingTexture = 0f;
+
+                float shadowRatio = AnalyseTexture();
+                targetColor = new Color(shadowRatio, 0, 1 -shadowRatio, 1);
+
+                ReleaseTexture();
+            }
+            
+            timeHoldingTexture += Time.deltaTime;
+        }
     }
 
 	void OnDisable()
 	{
 		Destroy(renderCameraObject);
-		//DestroyImmediate(mat);
 	}
 
     /////////////////////////
@@ -107,7 +148,7 @@ public class CasterLamp : MonoBehaviour
     /// <summary>
     /// Renders the view as seen from the lamp object.
     /// </summary>
-    private float RenderLightView ()
+    private void RenderLightView ()
     {
         //  Initialize Render Textures.
         renderTex = RenderTexture.GetTemporary(resolution, resolution, 24);
@@ -123,7 +164,12 @@ public class CasterLamp : MonoBehaviour
         //  Render 
         renderCamera.RenderWithShader(replaceShader, "RenderType");
 
-        //  Transfer RenderTexture data to Texture2D.
+        //isHoldingTexture = true;
+    }
+
+    private float AnalyseTexture ()
+    {
+        //  Transfer RenderTexture data to Texture2D by reading it to the active RenderTexture.
         RenderTexture.active = renderTex;
 
         resultImage.ReadPixels(new Rect(0, 0, resolution, resolution), 0, 0);
@@ -131,8 +177,7 @@ public class CasterLamp : MonoBehaviour
 
         RenderTexture.active = null;
 
-        //quad.GetComponent<Renderer>().material.mainTexture = resultImage;
-
+        //  Compute and return the ratio between shadow (yellow) and light (red) pixels on the player.
         Color[] pixels = resultImage.GetPixels();
 
         int green = 0;
@@ -150,13 +195,16 @@ public class CasterLamp : MonoBehaviour
             }
         }
 
-        //player.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, (float)red / (pixels.Length - green));
+        return (float)red / (pixels.Length - green);
+    }
 
+    private void ReleaseTexture ()
+    {
         //	Release and clear the RenderTextures.
         Camera.main.targetTexture = null;
         RenderTexture.ReleaseTemporary(renderTex);
         renderTex = null;
 
-        return (float)red / (pixels.Length - green);
+        //isHoldingTexture = false;
     }
 }
