0cd7b2ac1e662ba632423e16d8ef1023b52006c4
Abe Vos <abe_vos@msn.com>
2016-01-15 13:51:14 +0100

    Start on light manager to do all light/shadow checking in the scene.

---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LightManager : MonoBehaviour
+{
+
+    /// <summary>Generic List containing all lights currently used in the scene.</summary>
+    private List<Light> lights;
+
+    void Awake ()
+    {
+        //  TODO: Subscribe to delegates (e.g.: StateManager).
+
+        lights = new List<Light>();
+    }
+
+	////////////////////////
+    //  Public Functions  //
+    ////////////////////////
+
+    public void AddLight (Light light)
+    {
+        lights.Add(light);
+    }
+
+    public void RemoveLight (Light light)
+    {
+        lights.Remove(light);
+    }
+}
